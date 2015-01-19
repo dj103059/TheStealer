f041a55c1387ef7dee053d2e4e40c3b1854e346a
Cyrus8 <c.boumedine@hotmail.fr>
2015-01-19 18:22:24 +0100

    12 ème post
    modification Classe Map

--- src/item/Map.java
+++ src/item/Map.java
@@ -1,20 +1,43 @@
 package item;
-
+import room.*;
 /**
  * The box item
  **/
 public class Map extends Item {
 	//constant weight of the map
 	private static final int weigth = 5 ;
+	private Room [][] tabRoom;
+	
 	//constructor
-	public Map (){
-		this.setDescription(" you can see the area.");
+	public Map (Room [][] allRoom){
+		this.setDescription(" You can see the area.");
 		this.setName("Map");
 		this.setWeigth(weigth);
+		tabRoom=allRoom;
+		
+	}
+	
+	public void update(Room [][] allRoom){
+		
+		tabRoom=allRoom;
+		
 	}
 	
 	@Override
-	public void use(){
-		/* TODO  : during three turns , player's noise equal 0*/
+	public String use(){
+		String s;
+		for (int i=0;i<tabRoom.length;i++)
+		{
+			for (int j=0;j<tabRoom[i].length;j++)
+			{
+				for (int k=0;k<tabRoom[i][j].getListofentity().size();k++)
+				{
+					s="The entity "+tabRoom[i][j].getListofentity().get(k)+" is in the room ( "+i+", "+j+")"+"\n"+s;
+				}
+			}
+		}
+
+		return s;
+		
 	}
 }
