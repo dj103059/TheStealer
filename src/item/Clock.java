32ce7539b2719ee48488ae309ba5524cc9666a0b
dj103059 <dj103059@etu-unice.fr>
2015-01-19 18:20:28 +0100

    11 ieme post
    classe clock faite

--- src/item/Clock.java
+++ src/item/Clock.java
@@ -1,5 +1,36 @@
 package item;
 
-public class Clock extends Item{
+import timer.Timer;
 
+public class Clock extends Item{
+	// constant for the weigth of the box
+		private static final int weigth = 1 ;
+		private Timer time;
+
+
+		//constructor
+		public Clock (){
+			this.setDescription(" you can see the time remaining");
+			this.setName("wristwatch");
+			this.setWeigth(weigth);
+		}
+		
+		@Override
+		public String use(){
+			return "Time Remaining "+this.time.getTimer()+" min /n";
+		}
+		public Timer getTime() {
+			return time;
+		}
+
+		public void setTime(Timer time) {
+			this.time = time;
+		}
+		
+		public void update(int n){
+			this.time.setTimer(this.time.getTimer()-n);
+			if(n == 0){
+				//TODO
+			}
+		}
 }
