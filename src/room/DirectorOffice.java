c9850401929135622d181b5b7a7707cad2090440
dj103059 <dj103059@etu-unice.fr>
2015-01-19 19:33:15 +0100

    Revert "Revert "24ieme post""
    This reverts commit ff1fbc8ee74031b6fdf2b2c36950aa360800d16d.

--- src/room/DirectorOffice.java
+++ src/room/DirectorOffice.java
@@ -1,5 +1,15 @@
 package room;
 
+import item.Pass;
+
 public class DirectorOffice extends RoomPass{
+
+	public DirectorOffice(String description, String name) {
+		super(description, name);
+		this.setPass1(new Pass("Director Office"));
+	    this.addItem(new Pass("Gold Room"));
+	}
+	
+	
 
 }
--- src/room/RoomGold.java
+++ src/room/RoomGold.java
@@ -1,5 +1,12 @@
 package room;
 
+import item.Pass;
+
 public class RoomGold extends RoomPass{
+
+	public RoomGold(String description, String name) {
+		super(description, name);
+		this.setPass1(new Pass("Gold Room"));
+	}
 
 }
