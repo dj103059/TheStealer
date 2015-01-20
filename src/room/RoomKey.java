b33dfd57abcae27f366d81e472fb9fb6008c433b
Cyrus8 <c.boumedine@hotmail.fr>
2015-01-20 09:35:00 +0100

    32 ème post
    Classe RoomKey

--- src/room/RoomKey.java
+++ src/room/RoomKey.java
@@ -1,5 +1,37 @@
 package room;
 
-public class RoomKey {
+
+
+public class RoomKey extends Room {
+	
+	/**
+	 *  Create a RoomKey with the name and the description chosen
+	 */
+	public RoomKey(String description,String name){
+		super(description, name);
+		
+	}
+	
+	/**
+	 * Create a RoomKey with the name and the description chosen and add badge required to open the RoomPass placed in parameter
+	 * 
+	 * @param description
+	 * @param name
+	 * @param room
+	 */
+	public RoomKey(String description, String name,RoomPass room)
+	{
+		super(description,name);
+		this.addKey(room);
+		
+	}
+	/**
+	 * Add in this the Pass required to open the RoomPass placed in parameter
+	 * @param room
+	 */
+	public void addKey(RoomPass room)
+	{
+		this.addItem(room.getPass1());
+	}
 
 }
