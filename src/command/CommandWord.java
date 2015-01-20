ecc742babce66c689c2053bc2529fc1fd6bddf5f
Lydwen <tomlau94@aol.com>
2015-01-20 08:26:07 +0100

    26 éme post Move devient Go Ajout de CommandWord et CommandWords

--- src/command/CommandWord.java
+++ src/command/CommandWord.java
@@ -1,5 +1,31 @@
 package command;
 
-public class CommandWord {
+/**
+ * Representations of all the CommandWord which exists.
+ * @author Tom Dall'Agnol
+ */
+public enum CommandWord {
 
+    GO("go",new Go());
+    
+    // The command string.
+    private String commandString;
+    private Command commandWord;
+    /**
+     * Initialize with the corresponding command string.
+     * 
+     * @param commandString
+     *            The command string.
+     */
+    CommandWord(String commandString, Command command){
+        this.commandString = commandString;
+        this.commandWord = command;
+    }
+
+    /**
+     * @return The command word as a string.
+     */
+    public String toString() {
+        return commandString;
+    }
 }
--- src/command/Go.java
+++ src/command/Go.java
@@ -1,5 +1,5 @@
 package command;
 
-public class Move {
+public class Go {
 
 }
