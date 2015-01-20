c7bb6131e169c621afd1eb924130d13fc7976815
Cyrus8 <c.boumedine@hotmail.fr>
2015-01-20 10:50:27 +0100

    37ème
    Classe RoomGold

--- src/room/RoomGold.java
+++ src/room/RoomGold.java
@@ -3,10 +3,30 @@ package room;
 import item.Pass;
 
 public class RoomGold extends RoomPass{
+	private int gold;
 
 	public RoomGold(String description, String name) {
 		super(description, name);
 		this.setPass1(new Pass("Gold Room"));
+		this.setGold(0);
+	}
+	
+	public RoomGold(String description, String name, int g)
+	{
+		super(description, name);
+		this.setPass1(new Pass("Gold Room"));
+		this.setGold(g);
+	}
+	
+	public void setGold(int g)
+	{
+		this.gold=g;
 	}
 
+	public int getGold()
+	{
+		return gold;
+	}
+	
+	
 }
