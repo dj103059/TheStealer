a2e5a73feffb755f060157fd6b39fda2be70f793
PsYchoOosE <guillayme_fdr@orange.fr>
2015-01-20 10:33:38 +0100

    35éme post
    Fonctions d'ajout/suppression d'objets/gold

--- src/entity/Player.java
+++ src/entity/Player.java
@@ -6,6 +6,8 @@ import java.util.ArrayList;
 public class Player extends Entity{
 	private boolean hidden;
 	private ArrayList<Item> inventory;
+	private int gold=0;
+	private int goldWeight=4;
 	private int weight=0;
 	private int maxWeight;
 	
@@ -13,6 +15,50 @@ public class Player extends Entity{
 	public Player(int maxWeight){
 		this.maxWeight=maxWeight;
 		hidden=false;
+	}
+	
+	// Methods
+	/**
+	 * 
+	 * @param i		Item
+	 * @param gold	Amount of gold
+	 * @param add	True if you add, false if you remove
+	 * @return		The new weight 
+	 */
+	private int calculateWeight(Item i, int gold, boolean add){
+		int signe=1;;
+		if (!add){signe=-1;}
+		int weight=gold*goldWeight;
+		if (i!=null){weight+=signe*i.getWeight();}
+		if (gold !=0){weight+=signe*gold*goldWeight;}
+		return weight;
+	}
+	/**
+	 * 
+	 * @param i		Item you want to add in the player inventory
+	 * @param gold	Amount of gold you want to add to the player
+	 * @return		False if he can't carry this weight
+	 */
+	public boolean add(Item i, int gold){
+		int tmp=calculateWeight(i, gold, true);
+		if (tmp<=maxWeight){
+			if (i!=null){
+				inventory.add(i);
+			}
+			this.gold+=gold;
+			weight=tmp;
+			return true;
+		}else{return false;}
+	}
+	/**
+	 * 
+	 * @param i		Item the player want to drop
+	 * @param gold	Amount of gold the player want to drop;
+	 */
+	public void drop(Item i, int gold){
+		for (Item j : inventory){if (j.equals(i)){inventory.remove(j);}}
+		this.gold-=gold;
+		weight=calculateWeight(i,gold,false);
 	}
 	
 	// Getters
