315e7e47c899370a17e003c719ad5633fa4e7969
Cyrus8 <c.boumedine@hotmail.fr>
2015-01-20 11:49:48 +0100

    42ème
    commentaire RoomGold

--- src/room/RoomGold.java
+++ src/room/RoomGold.java
@@ -2,25 +2,55 @@ package room;
 
 
 /**
- * the treasure room
+ * The treasure room
  *
  */
 public class RoomGold extends RoomPass{
 	//the amont of gold
+	
+	/**
+	 * The gold that the room contains
+	 */
 	private int gold;
 
 	//constructor
+	
+	/**
+	 * Create a RoomGold with a description, a name and an amount of gold of 0.
+	 * 
+	 * @param description the description of the room
+	 * @param name name of the room
+	 */
 	public RoomGold(String description, String name) {
 		super(description, name);
 		this.setGold(0);
 	}
 	
+	/**
+	 * Create a RoomGold with the amount of gold given in parameter
+	 * 
+	 * @param description the description of the room
+	 * @param name the name of the room
+	 * @param g the gold in the room
+	 */
 	//the constructor with the amont of gold in the room
+	
+	/**
+	 * 
+	 * @param description
+	 * @param name
+	 * @param g gold in the room
+	 */
 	public RoomGold(String description, String name, int g)
 	{
 		super(description, name);
 		this.setGold(g);
 	}
+	
+	/**
+	 * Change the amount of the gold in the room
+	 * @param g
+	 */
 	
 	//getter and setter
 	public void setGold(int g)
@@ -28,6 +58,10 @@ public class RoomGold extends RoomPass{
 		this.gold=g;
 	}
 
+	/**
+	 * 
+	 * @return the amount of the gold in the room.
+	 */
 	public int getGold()
 	{
 		return gold;
